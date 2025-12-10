b7ec971e4c7a4ccd37a625bab1a34865920a91f4
netsong7 <netsong7naver.com>
2025-12-10 09:34:32 +0900

    최초 작성

---
+++ projects/spring-app/src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.Employee;
+import com.example.demo.service.EmployeeService;
+
+@RestController
+@RequestMapping("/api/v1/")
+@CrossOrigin()
+class EmployeeController {   
+	@Autowired
+    private EmployeeService employeeService;
+
+	@GetMapping("/")
+    public String getIndex() {
+        return "<h1>안녕하세요</h1> 수정했습니다";
+    }
+	
+    @GetMapping("/employees")
+    public Employee getAll() {
+        return employeeService.getAll();
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.add(employee);
+    }
+}
